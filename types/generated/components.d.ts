74f58d8514502679dee0efde8aa98d990ef31f00
Akhilesh Jha <akhilesh@sentientgeeks.com>
2024-08-13 23:12:29 +0530

    New field add

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -1,5 +1,19 @@
 import type { Schema, Attribute } from '@strapi/strapi';
 
+export interface SectionTest extends Schema.Component {
+  collectionName: 'components_section_tests';
+  info: {
+    displayName: 'Test';
+  };
+  attributes: {
+    test: Attribute.Password;
+  };
+}
+
 declare module '@strapi/types' {
-  export module Shared {}
+  export module Shared {
+    export interface Components {
+      'section.test': SectionTest;
+    }
+  }
 }
